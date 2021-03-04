fd46e28903cf419884cf817af646d267e06d210e
Rithe <richard.rabeder@gmail.com>
2021-03-04 20:16:50 +0100

    add onclicklisteners to food fragment

--- app/src/main/java/com/candybytes/taco/ui/fragments/FoodFragment.kt
+++ app/src/main/java/com/candybytes/taco/ui/fragments/FoodFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.candybytes.taco.databinding.FragmentFoodBinding
 import com.candybytes.taco.ui.adapters.NutrientAdapter
+import timber.log.Timber
 
 class FoodFragment : Fragment() {
 
@@ -20,6 +22,7 @@ class FoodFragment : Fragment() {
         return FragmentFoodBinding.inflate(inflater, container, false).apply {
             adapter = NutrientAdapter()
             foodNutritionList.adapter = adapter
+            setOnClickListeners()
 
         }.root
     }
@@ -28,5 +31,16 @@ class FoodFragment : Fragment() {
         val args: FoodFragmentArgs by navArgs()
         val food = args.food
         adapter.submitList(food.nutrients.toList())
+    }
+
+    private fun FragmentFoodBinding.setOnClickListeners() {
+        setClickListener {
+            when (it) {
+                categoryBtn -> findNavController().navigate(
+                    FoodFragmentDirections.actionFoodFragmentToCategoryFragment()
+                )
+                imageAddBtn -> Timber.d("add image")
+            }
+        }
     }
 }
